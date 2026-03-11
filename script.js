2fdedf677902a6d1e96e323383001e2a0b9a4f8a
kaafall <uk.kaafall@gmail.com>
2026-03-11 15:39:52 +0530

    update3

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ document.getElementById("splash").style.display="none";
 document.getElementById("main-content").style.display="block";
 
 },2500);
+
+// -------------------------------------------------------------------------------------
+// contact us page form submission
+
+document.getElementById("contactForm").addEventListener("submit",function(e){
+
+e.preventDefault();
+
+alert("Message Sent Successfully!");
+
+});
+
+// -------------------------------------------------------------------------------------
