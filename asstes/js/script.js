4d009842911c6881b81853c13ba6761dfe0c3ac9
kaafall <uk.kaafall@gmail.com>
2026-07-06 18:37:17 +0530

    dggfgfgfgfg

--- asstes/js/script.js
+++ asstes/js/script.js
@@ -2,7 +2,23 @@ async function loadComponent(id, file) {
     const response = await fetch(file);
     const data = await response.text();
     document.getElementById(id).innerHTML = data;
+
+    if(id === "header"){
+        initMenu();
+    }
 }
 
-loadComponent("header", "header.html");
-loadComponent("footer", "footer.html");
+function initMenu(){
+    const hamburger = document.getElementById("hamburger");
+    const navLinks = document.getElementById("navLinks");
+
+    if(hamburger && navLinks){
+        hamburger.addEventListener("click", () => {
+            navLinks.classList.toggle("active");
+            hamburger.classList.toggle("active");
+        });
+    }
+}
+
+loadComponent("header","header.html");
+loadComponent("footer","footer.html");
